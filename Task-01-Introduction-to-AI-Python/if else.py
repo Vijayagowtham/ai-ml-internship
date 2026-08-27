daf0ae2af07cba6ac861f94a1be2722ba66f2c63
Vijayagowtham <vijayagowthamv@gmail.com>
2026-08-27 20:17:06 +0530

    Update project changes

---
+++ Task-01-Introduction-to-AI-Python/if else.py
@@ -0,0 +1,11 @@
+n =input(int("Enter the number:"))
+if n %2!=0:
+    print("Weird")
+
+elif 2<=n<=5 & n%2==0:
+    print("Not Weird") 
+elif 6<=n<=20 & n%2==0:
+    print("Weird")   
+
+else n>20 & %2==0:
+    print("Not Weird")
